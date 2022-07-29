d2f6b48c44b2fdbc1d33250fd2dadf817ada324a
Tamia Quiyara <mia.mbuckingworth@gmail.com>
2022-07-29 13:41:13 +0200

    Corrected spelling error

--- test/greet.test.js
+++ test/greet.test.js
@@ -4,7 +4,7 @@ let greet = require("../greet");
 describe('The greet function', function(){
 
     it('should greet Andrew correctly', function(){
-        assert.equal('Hello, Andre', greet('Andre'));
+        assert.equal('Hello, Andrew', greet('Andrew'));
     });
     it('should greet Karen correctly', function(){
         // this test will fail - can you fix it?
